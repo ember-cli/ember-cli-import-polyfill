2d1d1340e47b23eb1b6adc27222119cab451b1d9
Edward Faulkner <ef@alum.mit.edu>
2016-05-06 23:17:39 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 /* jshint node: true */
 'use strict';
 
+var Addon = require('ember-cli/lib/models/addon');
+
+if (!Addon.prototype.import) {
+  Addon.prototype.import = function(asset, options) {
+    var app = this.app;
+    while (app.app) {
+      app = app.app;
+    }
+    app.import(asset, options);
+  };
+}
+
 module.exports = {
   name: 'ember-cli-import-polyfill'
 };
